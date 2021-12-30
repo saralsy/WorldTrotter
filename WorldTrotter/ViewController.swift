fa94bc12b80338c89674a4518ae767dac9a80f53
Sara Siye Liu <sara.liu@nyu.edu>
2021-12-30 12:09:22 -0600

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Sara Liu on 12/30/21.
+//  Copyright © 2021 Sara Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
